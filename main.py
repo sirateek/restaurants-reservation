fe2d89f64773e14d5ccc5a0fbff79d0be7565ea9
rattatham <alimokung@gmail.com>
2022-02-06 15:21:15 +0700

    get by name and table

--- main.py
+++ main.py
@@ -10,22 +10,28 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["restaurant"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["reservation"]
+menu_collection=db["reservation"]
 
 app = FastAPI()
-
 
 # TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
-    pass
+    query={"name":name}
+    query_result=menu_collection.find(query)
+    for n in query_result:
+        print(n)
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
-    pass
+    query={"table_number":table}
+    query_result=menu_collection.find(query)
+    for n in query_result:
+        print(n)
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
